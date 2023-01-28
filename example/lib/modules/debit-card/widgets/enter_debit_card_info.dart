204e655590bca090644978ad0b2e809c87ab2f6f
Obinna Onuoha <onuohasilver9@gmail.com>
2023-01-28 10:16:42 +0100

    ➕ add multi-formatter dependency

--- example/lib/modules/debit-card/widgets/enter_debit_card_info.dart
+++ example/lib/modules/debit-card/widgets/enter_debit_card_info.dart
@@ -1,6 +1,9 @@
+import 'package:example/models/models.dart';
 import 'package:example/modules/-core-global/-core-global.dart';
 import 'package:example/modules/debit-card/controllers/debit_card_notifier.dart';
+import 'package:example/modules/view-notifiers/view_notifier.dart';
 import 'package:example/modules/view-notifiers/view_state.dart';
+import 'package:example/modules/widgets/amount_to_pay.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,45 +15,47 @@ class EnterDebitCardInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     DebitCardNotifier dcn = Provider.of<DebitCardNotifier>(context);
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const YSpace(12),
-        const CustomText("NGN 100.00", weight: FontWeight.bold, size: 24),
-        const YSpace(8),
-        const CustomText("Fee: NGN1.50", size: 14),
-        const YSpace(32),
-        const CustomTextField(
-            label: "Debit/credit card details", hint: "Card number"),
-        Row(
-          children: const [
-            Expanded(
-              child: CustomTextField(
-                label: "",
-                hint: "MM/YY",
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    bottomLeft: Radius.circular(10)),
-              ),
-            ),
-            Expanded(
+    ViewsNotifier vn = Provider.of<ViewsNotifier>(context);
+    return Builder(builder: (context) {
+      MerchantDetailModel mdm = vn.merchantDetailModel!;
+      PaymentPayloadModel ppm = vn.paymentPayload!;
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AmountToPay(fee: mdm.payload.cardFee.mc!),
+          const YSpace(32),
+          const CustomTextField(
+              label: "Debit/credit card details", hint: "Card number"),
+          Row(
+            children: const [
+              Expanded(
                 child: CustomTextField(
-              label: "",
-              hint: "CVV",
-              borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(10),
-                  bottomRight: Radius.circular(10)),
-            )),
-          ],
-        ),
-        const YSpace(20),
-        CustomFlatButton(
-            label: "Continue to Payment",
-            onTap: () => dcn.changeView(CurrentCardView.pin),
-            expand: true,
-            color: Colors.white54,
-            bgColor: Colors.grey),
-      ],
-    );
+                  label: "",
+                  hint: "MM/YY",
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10)),
+                ),
+              ),
+              Expanded(
+                  child: CustomTextField(
+                label: "",
+                hint: "CVV",
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(10),
+                    bottomRight: Radius.circular(10)),
+              )),
+            ],
+          ),
+          const YSpace(20),
+          CustomFlatButton(
+              label: "PAY NGN ${ppm.amount}",
+              onTap: () => dcn.changeView(CurrentCardView.pin),
+              expand: true,
+              color: Colors.white54,
+              bgColor: Colors.grey),
+        ],
+      );
+    });
   }
 }
