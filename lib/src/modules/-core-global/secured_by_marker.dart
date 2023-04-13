006c899fc4ccb978c354df41046c2cc19dc9e4da
Obinna Onuoha <onuohasilver9@gmail.com>
2023-04-13 12:35:08 +0100

    change secured_by_marker to netwrk url

--- lib/src/modules/-core-global/secured_by_marker.dart
+++ lib/src/modules/-core-global/secured_by_marker.dart
@@ -1,7 +1,6 @@
-
-import 'package:seerbit_flutter_native/src/modules/-core-global/-core-global.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:seerbit_flutter_native/src/modules/-core-global/-core-global.dart';
 
 class SecuredByMarker extends StatelessWidget {
   const SecuredByMarker({
@@ -17,8 +16,8 @@ class SecuredByMarker extends StatelessWidget {
         const XSpace(5),
         const CustomText("Secured by", size: 14),
         const XSpace(5),
-        Image.asset(
-          "assets/seerbit.png",
+        Image.network(
+          "https://raw.githubusercontent.com/seerbit/seerbit-flutter-native/2c1339290a4b73fc8ea4f1057080f3dc9572427b/assets/seerbit.png?token=GHSAT0AAAAAAB3U6CZ6UHHQONELJRE3HL4EZBX5DAA",
           width: 60,
         )
       ],
